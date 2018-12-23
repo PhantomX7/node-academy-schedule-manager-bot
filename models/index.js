93b0ad6f64bb6edd0e86e16881a06eb87d80c4a5
delanobgt <irvindelano@gmail.com>
2018-12-23 21:52:11 +0700

    default db url

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/linebot'
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/academy-schedule-manager-bot'
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
 
 exports.Event = require('./event')
